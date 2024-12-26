dac0ce0d4f51f770c60641cd4d5b4369c0a50aaf
v-aria-ble <ariaahnaf.s@gmail.com>
2024-12-26 19:59:12 +0700

    Initial commit

---
+++ app/src/main/java/com/itnews/texttospeechandreverseapp/MainActivity.java
@@ -0,0 +1,93 @@
+package com.itnews.texttospeechandreverseapp;
+
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.speech.RecognizerIntent;
+import android.speech.tts.TextToSpeech;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Locale;
+import java.util.Objects;
+
+public class MainActivity extends AppCompatActivity {
+    TextToSpeech textToSpeech;
+    Button readBtn, speakBtn;
+    TextView textRead;
+
+    private static final int REQUEST_CODE_SPEECH_INPUT = 1;
+
+    private void init(){
+        readBtn = findViewById(R.id.readBtn);
+        speakBtn = findViewById(R.id.speakBtn);
+        textRead = findViewById(R.id.textRead);
+        textToSpeech = new TextToSpeech(getApplicationContext(), new TextToSpeech.OnInitListener() {
+            @Override
+            public void onInit(int status) {
+                if(status!=TextToSpeech.ERROR){
+                    textToSpeech.setLanguage(Locale.ENGLISH);
+                }
+            }
+        });
+        readBtn.setOnClickListener(e->{
+            textToSpeech.speak(textRead.getText().toString(), TextToSpeech.QUEUE_FLUSH, null);
+        });
+        speakBtn.setOnClickListener(e->{
+            Intent intent
+                    = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                    RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+            intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                    Locale.getDefault());
+            intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
+
+            try {
+                startActivityForResult(intent, REQUEST_CODE_SPEECH_INPUT);
+            }
+            catch (Exception i) {
+                Toast
+                        .makeText(MainActivity.this, " " + i.getMessage(),
+                                Toast.LENGTH_SHORT)
+                        .show();
+            }
+        });
+    }
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        init();
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.RECORD_AUDIO},
+                    REQUEST_CODE_SPEECH_INPUT);
+        }
+    }
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode,
+                                    @Nullable Intent data)
+    {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE_SPEECH_INPUT) {
+            if (resultCode == RESULT_OK && data != null) {
+                ArrayList<String> result = data.getStringArrayListExtra(
+                        RecognizerIntent.EXTRA_RESULTS);
+                textRead.setText(
+                        Objects.requireNonNull(result).get(0));
+            }
+        }
+    }
+}
